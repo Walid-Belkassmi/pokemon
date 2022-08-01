ff292a10074157c464af0b7af2a10ad77df174e1
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-08-01 19:56:34 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import * as React from "react";
+// import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  const getRandomPokemon = () => {
+    const random = Math.floor(Math.random() * 151) + 1;
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${random}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="display">
+      <div className="card">
+        <img
+          src={pokemon.sprites.other["official-artwork"].front_default}
+          alt="pokemon"
+        />
+        <h1>{pokemon.name}</h1>
+        <h2>Height : {pokemon.height}</h2>
+        <h2>Weight : {pokemon.weight}</h2>
+        <h2>Types:</h2>
+        <ul>
+          {pokemon.types.map((type) => (
+            <li>{type.type.name}</li>
+          ))}
+        </ul>
+      </div>
+      <button onClick={getRandomPokemon}>Generate random pokemon</button>
     </div>
   );
-}
+};
 
 export default App;
